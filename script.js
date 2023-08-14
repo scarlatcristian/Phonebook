01864e132f6800666e77b9cbc04dd880e9d493a9
cristi <scarlat.cristian.gabriel@gmail.com>
2023-08-14 17:28:00 +0300

    added delete btn and show keypad on click

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+const addNumberBtn = document.querySelector(".add-number");
+
+const display = document.querySelector(".display");
+const keypadBtn = document.querySelectorAll(".keypad");
+const deleteBtn = document.querySelector(".delete");
+
+let phoneNumberDisplay = "";
+
+keypadBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    display.textContent += btn.textContent;
+    if (display.textContent !== "") {
+      addNumberBtn.style.opacity = 1;
+      deleteBtn.style.opacity = 1;
+    }
+  });
+});
+
+deleteBtn.addEventListener("click", () => {
+  display.textContent = display.textContent.slice(0, -1);
+  if (display.textContent === "") {
+    addNumberBtn.style.opacity = 0;
+    deleteBtn.style.opacity = 0;
+  }
+});

06f12cbc15f6dc4b74533722e9092022f4ed736c
cristi <scarlat.cristian.gabriel@gmail.com>
2023-08-15 15:40:01 +0300

    create save contact page

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 "use strict";
-const addNumberBtn = document.querySelector(".add-number");
+// PAGES
+const addNumberContainer = document.querySelector(".keypad-container");
+const createContact = document.querySelector(".create-contact");
+console.log(createContact);
 
+// Add Number Container
+const addNumberBtn = document.querySelector(".add-number");
 const display = document.querySelector(".display");
 const keypadBtn = document.querySelectorAll(".keypad");
 const deleteBtn = document.querySelector(".delete");
 
-let phoneNumberDisplay = "";
+// Create Contact
+const inputName = document.querySelector(".input_name");
+const inputFirstName = document.querySelector(".input_firstName");
+const inputNumber = document.querySelector(".input_number");
+
+let phoneNumber = "";
 
 keypadBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     display.textContent += btn.textContent;
+    phoneNumber = display.textContent;
     if (display.textContent !== "") {
       addNumberBtn.style.opacity = 1;
       deleteBtn.style.opacity = 1;
@@ -23,4 +34,10 @@ deleteBtn.addEventListener("click", () => {
     addNumberBtn.style.opacity = 0;
     deleteBtn.style.opacity = 0;
   }
+});
+
+addNumberBtn.addEventListener("click", () => {
+  inputNumber.value = phoneNumber;
+  addNumberContainer.classList.add("hidden");
+  createContact.classList.remove("hidden");
 });

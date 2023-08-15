5f0126f3c4cf542fd7211d6cdf6d037879365f1f
cristi <scarlat.cristian.gabriel@gmail.com>
2023-08-15 15:45:56 +0300

    fix bug number not saved after delete

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // PAGES
 const addNumberContainer = document.querySelector(".keypad-container");
 const createContact = document.querySelector(".create-contact");
-console.log(createContact);
 
 // Add Number Container
 const addNumberBtn = document.querySelector(".add-number");
@@ -14,6 +13,8 @@ const deleteBtn = document.querySelector(".delete");
 const inputName = document.querySelector(".input_name");
 const inputFirstName = document.querySelector(".input_firstName");
 const inputNumber = document.querySelector(".input_number");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
 
 let phoneNumber = "";
 
@@ -21,6 +22,7 @@ keypadBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     display.textContent += btn.textContent;
     phoneNumber = display.textContent;
+
     if (display.textContent !== "") {
       addNumberBtn.style.opacity = 1;
       deleteBtn.style.opacity = 1;
@@ -30,6 +32,8 @@ keypadBtn.forEach((btn) => {
 
 deleteBtn.addEventListener("click", () => {
   display.textContent = display.textContent.slice(0, -1);
+  phoneNumber = display.textContent;
+
   if (display.textContent === "") {
     addNumberBtn.style.opacity = 0;
     deleteBtn.style.opacity = 0;
